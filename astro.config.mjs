65d1f8f0ef5a913d04439cbe32f6dcce1a2ad2ec
monodyle <monodylh@gmail.com>
2024-11-18 23:09:15 +0700

    cf adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'http://localhost:4321',
   output: 'hybrid',
-  integrations: [tailwind(), sitemap(), mdx()]
+  integrations: [tailwind(), sitemap(), mdx()],
+  adapter: cloudflare()
 });
